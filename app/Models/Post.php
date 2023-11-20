bf4c048ca2b180e4eb0c34e6e4b0661f00a8a37f
huuhieunguyen <huuhieu.nguyen0408@gmail.com>
2023-11-21 00:46:48 +0700

    finish post crud

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'sender_id',
+        'author_id',
+        'content',
+        'location',
+        'like_count',
+        // 'image_url',
+        // 'created_at',
+        // 'updated_at',
+    ];
+    
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
